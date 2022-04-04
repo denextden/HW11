74b0634baf55ca8ca4d04a6fb0226cab9d8476ba
denextden <extenden98@gmail.com>
2022-04-04 12:42:02 +0300

    changed library because outdated

---
+++ HW11.2/main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+from utils import load_candidates_from_json, get_candidate, get_candidates_by_name, get_candidates_by_skill
+
+app = Flask(__name__)
+
+data = load_candidates_from_json('candidates.json')
+
+
+@app.route('/')
+def index():
+    return render_template('list.html', candidates=data)
+
+
+@app.route('/candidate/<int:uid>')
+def card(uid):
+    candidate = get_candidate(uid)
+    return render_template('card.html', candidate=candidate)
+
+
+@app.route('/search/<name>')
+def search(name):
+    candidates = get_candidates_by_name(name)
+    print(candidates)
+    return render_template('search.html', candidates=candidates, candidates_len=len(candidates))
+
+
+@app.route('/skills/<skill>')
+def skills(skill):
+    candidates = get_candidates_by_skill(skill)
+
+    return render_template('skill.html', candidates=candidates, candidates_len=len(candidates), skill=skill)
+
+
+app.run()
